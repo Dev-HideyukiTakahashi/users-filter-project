e85b3fc2babf55668b974426b5c55a4d9a3cd15b
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-03-07 05:18:50 -0300

    create user lists (table)

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,10 +4,11 @@ import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-  declarations: [UserDetailsComponent, FilterComponent],
+  declarations: [UserDetailsComponent, FilterComponent, UsersListComponent],
   imports: [AngularMaterialModule, FormsModule, CommonModule],
-  exports: [UserDetailsComponent, FilterComponent],
+  exports: [UserDetailsComponent, FilterComponent, UsersListComponent],
 })
 export class ComponentsModule {}
