75567618e1d581b2a35fde610e408c43b55362f5
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-03-06 22:53:36 -0300

    config modules/angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'users-filter-project';
-}
+export class AppComponent {}
